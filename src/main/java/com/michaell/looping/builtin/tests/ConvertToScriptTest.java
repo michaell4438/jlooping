1ac6fd91659821d9924808d43f85bf43a87ae06a
MichaelL4438 <michael@lachut.com>
2022-05-26 15:06:03 -0400

    Added ConvertToScript and some tests for it

---
+++ src/main/java/com/michaell/looping/builtin/tests/ConvertToScriptTest.java
@@ -0,0 +1,10 @@
+package com.michaell.looping.builtin.tests;
+
+public class ConvertToScriptTest {
+    public void start() {
+        System.out.println("Bonjour, mes amis!");
+    }
+    public void main() {
+        System.out.println("J'ai besoin d'une baguette");
+    }
+}
--- src/main/java/com/michaell/looping/builtin/tests/HelloWorldTest.java
+++ src/main/java/com/michaell/looping/builtin/tests/HelloWorldTest.java
@@ -2,12 +2,13 @@ package com.michaell.looping.builtin.tests;
 
 import com.michaell.looping.ScriptParameters;
 import com.michaell.looping.ScriptRunner;
+import com.michaell.looping.builtin.ConvertToScript;
 
 public class HelloWorldTest {
     public static void main(String[] args) {
         ScriptRunner runner = new ScriptRunner();
         runner.addRequest(new HelloWorldRequest());
-        ScriptParameters.GlobalVariable<Integer> var = new ScriptParameters.GlobalVariable<Integer>("counter");
+        ScriptParameters.GlobalVariable<Integer> var = new ScriptParameters.GlobalVariable<>("counter");
         var.setValue(0);
         runner.scriptParametersGlobal.addGlobalVariable(var);
         try {
@@ -17,7 +18,8 @@ public class HelloWorldTest {
             runner.addScript(new RecursiveScript("Recursive1"));
             runner.addScript(new HelloWorldScript("Hello4"));
             runner.addScript(new HelloWorldScript("Hello5"));
-        } catch (ScriptRunner.DuplicateScriptException e) {
+            runner.addScript(new ConvertToScript("Francais", new ConvertToScriptTest(), "start", "main"));
+        } catch (ScriptRunner.DuplicateScriptException | NoSuchMethodException e) {
             throw new RuntimeException(e);
         }
         runner.runConstantly(10000);
