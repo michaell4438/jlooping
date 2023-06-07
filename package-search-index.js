a6db1c99c20700983b0a93d929a98d196f92c4b8
Thomas Ricci <git@thomasricci.dev>
2023-06-07 12:44:28 -0400

    javadoc

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.michaell.looping"},{"l":"com.michaell.looping.builtin"},{"l":"com.michaell.looping.builtin.tests"}]
